e5a2fb5b7b62d114b4f76b64e8d36c4304439af8
Isaac Hili <18428880+isaachili@users.noreply.github.com>
2022-09-03 13:25:18 +0200

    WIP: Snap scrolling and indicator synchronisation

--- src/Buddies.App/wwwroot/src/dom-functions.js
+++ src/Buddies.App/wwwroot/src/dom-functions.js
@@ -1,10 +1,69 @@
-﻿function scrollIntoView(id) {
+﻿const interactionObservers = {};
+
+function observeInteraction(id) {
+
+	// Don't track the same element more than once
+	if (!!interactionObservers[id]) {
+		return;
+	}
+
 	const element = document.getElementById(id);
 
 	if (!element) {
 		return;
 	}
 
+	// Create interaction observer
+	const observer = new IntersectionObserver(entries => {
+
+		if (!entries[0].isIntersecting) {
+			return;
+		}
+
+		// TODO - Dispatch event?
+	}, {
+		threshold: 1
+	});
+
+	// Observe element
+	observer.observe(element);
+
+	// Track observer for element
+	interactionObservers[id] = observer;
+}
+
+function unobserveInteraction(id) {
+
+	// Retrieve interaction observer
+	observer = interactionObservers[id];
+
+	if (!observer) {
+		return;
+	}
+
+	// Retrieve element
+	const element = document.getElementById(id);
+
+	if (!element) {
+		return;
+	}
+
+	// Unobserve element
+	observer.unobserve(element);
+
+	// Stop tracking observer
+	delete interactionObservers[id];
+}
+
+function scrollIntoView(id) {
+
+	const element = document.getElementById(id);
+
+	if (!element) {
+		return;
+	}
+
+	// Scroll element into view
 	element.scrollIntoView({
 		behavior: 'smooth',
 	});
